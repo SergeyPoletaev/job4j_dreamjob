d76bd3a9a61e6e98e80636e0da76c62066e0a3f4
spoletaev <info.elekit@yandex.ru>
2022-10-25 13:11:47 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -2,19 +2,20 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Post;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
+    private final AtomicInteger count = new AtomicInteger();
     private final static PostStore INST = new PostStore();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "msg1", LocalDateTime.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "msg2", LocalDateTime.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "msg3", LocalDateTime.now()));
+        posts.put(1, new Post(count.incrementAndGet(), "Junior Java Job", "msg1"));
+        posts.put(2, new Post(count.incrementAndGet(), "Middle Java Job", "msg2"));
+        posts.put(3, new Post(count.incrementAndGet(), "Senior Java Job", "msg3"));
     }
 
     public static PostStore instOf() {
@@ -23,5 +24,10 @@ public class PostStore {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public boolean add(Post post) {
+        post.setId(count.incrementAndGet());
+        return posts.putIfAbsent(post.getId(), post) == null;
     }
 }
